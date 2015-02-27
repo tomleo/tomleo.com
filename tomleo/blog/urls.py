ca2f07d0b5666ec8caec676a30b5f6194de6a050
Tom Leo <tom@tomleo.com>
2015-02-27 18:56:34 -0500

    Removed slug from URLconf
    The slug doesn't seem to be a rest_framework convension

--- tomleo/blog/urls.py
+++ tomleo/blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
 urlpatterns = [
-    url(r'^posts/(?P<pk>[0-9]+)/(?P<slug>\w+)/$', views.PostDetailView.as_view(), name='post')
+    url(r'^posts/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='post-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
