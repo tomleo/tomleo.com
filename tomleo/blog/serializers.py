ae29115bed568774e1a55ee19384b01a2b6c5fca
Tom Leo <tom@tomleo.com>
2015-02-27 18:53:15 -0500

    Added info from rest_framework docs about source argument

--- tomleo/blog/serializers.py
+++ tomleo/blog/serializers.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import User
 from .models import Post
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
+
     author = serializers.ReadOnlyField(source='author.username')
+    # source argument controls which attribute is used to populate a field, and
+    # can point at any attribute on the serialized instance. It can also take
+    # the dotted notation shown above, in which case it will traverse the given
+    # attributes, in a similar way as it is used with Django's template
+    # language
 
     class Meta:
         model = Post
